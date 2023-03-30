6fb8583e148007d2adcf39c3a3f1555ae63195ee
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-03-30 19:29:12 -0300

    Leitura da imagem

---
+++ alura-stickers/src/GeradoraDeFigurinhas.java
@@ -0,0 +1,23 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class GeradoraDeFigurinhas {
+    
+    public void cria() throws Exception {
+
+        // Leitura da imagem
+        BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpg"));
+        
+        // Cria nova imagem em memória com transparência e com tamanho novo
+
+        // Copiar a imagem original pra nova imagem (em memória)
+        
+        // Escrever uma frase na nova imagem
+
+        // Escrever a nova imagem em um arquivo
+        
+    }
+
+}
