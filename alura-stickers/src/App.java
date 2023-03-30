36fe939e7c755821b6b9af5e3eaec4c7056abb1c
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-03-30 19:11:08 -0300

    Desafios da aula 01

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -20,8 +20,21 @@ public class App {
         // Extrair só os dados que interessam(titulo, poster, nota, ano)
         JsonParser parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        System.out.println(listaDeFilmes.size());
-        
+
         // Exibir e manipular os dados
+
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println("\u001b[1mTítulo:\u001b[m " + filme.get("title"));
+            System.out.println("\u001b[1mPoster:\u001b[m " + filme.get("image"));
+            System.out.println(filme.get("year"));
+            double classificacao = Double.parseDouble(filme.get("imDbRating"));
+            System.out.println("\u001b[1m\u001b[44m\u001b[37mClassificação: " + classificacao + "\u001b[m");
+            int numeroEstrelinhas = (int) classificacao;
+            for (int i = 1; i < numeroEstrelinhas; i++) {
+                System.out.print("⭐");
+            }
+            System.out.println("\n");
+            
+        }
     }
 }
