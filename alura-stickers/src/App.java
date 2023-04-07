92074e1da4e592055916b1064c1f42c40a771a48
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-04-07 15:07:42 -0300

    Adicionando um Enum com as configurações

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -7,12 +7,10 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // Fazer uma conexão HTTP e buscar os top 250 filmes do IMDB
-        // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        // ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
+        API api = API.NASA;
+        String url = api.getUrl();
 
-        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
-        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+        ExtratorDeConteudo extrator = api.getExtrator();
 
         ClienteHttp http = new ClienteHttp();
         String json = http.buscaDados(url);
