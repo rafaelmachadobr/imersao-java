cd0a0b892bb809f8f692697b72b488e46483ef01
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-04-07 14:55:32 -0300

    Usando Streams e Lambdas para manipulação

--- alura-stickers/src/ExtratorDeConteudoDaNasa.java
+++ alura-stickers/src/ExtratorDeConteudoDaNasa.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -10,18 +9,8 @@ public class ExtratorDeConteudoDaNasa implements ExtratorDeConteudo {
         JsonParser parser = new JsonParser();
         List<Map<String, String>> listaDeAtributos = parser.parse(json);
 
-        List<Conteudo> conteudos = new ArrayList<>();
-
-        // Popular a lista de conteudos
-        for (Map<String, String> atributos : listaDeAtributos) {
-            String titulo = atributos.get("title");
-            String urlImagem = atributos.get("url");
-
-            Conteudo conteudo = new Conteudo(titulo, urlImagem);
-
-            conteudos.add(conteudo);
-        }
-
-        return conteudos;
+        return listaDeAtributos.stream()
+            .map(atributos -> new Conteudo(atributos.get("title"), atributos.get("url")))
+            .toList();
     }
 }
--- alura-stickers/src/ExtratorDeConteudoDoIMDB.java
+++ alura-stickers/src/ExtratorDeConteudoDoIMDB.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -10,18 +9,8 @@ public class ExtratorDeConteudoDoIMDB implements ExtratorDeConteudo {
         JsonParser parser = new JsonParser();
         List<Map<String, String>> listaDeAtributos = parser.parse(json);
 
-        List<Conteudo> conteudos = new ArrayList<>();
-
-        // Popular a lista de conteudos
-        for (Map<String, String> atributos : listaDeAtributos) {
-            String titulo = atributos.get("title");
-            String urlImagem = atributos.get("image");
-
-            Conteudo conteudo = new Conteudo(titulo, urlImagem);
-
-            conteudos.add(conteudo);
-        }
-
-        return conteudos;
+        return listaDeAtributos.stream()
+                .map(atributos -> new Conteudo(atributos.get("title"), atributos.get("image")))
+                .toList();
     }
 }
